6d2437c769aa795ec90a395ac13389904fafdbb3
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-29 18:13:33 +0200

    Done lesson001 home work

---
+++ Lesson001_HomeTask1/Lesson001_HomeTask1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson001_HomeTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pi = 3.141592653;
+            decimal e = 2.7182818284590452m;
+            Console.WriteLine("pi = {0} \ne = {1}", pi, e);
+        }
+    }
+}
---
+++ Lesson001_HomeTask2/Lesson001_HomeTask2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson001_HomeTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string myStr1 = "\nмій рядок 1";
+            string myStr2 = "\tмій рядок 2";
+            string myStr3 = "\aмій рядок 3";
+            Console.WriteLine(myStr1);
+            Console.WriteLine(myStr2);
+            Console.WriteLine(myStr3);
+        }
+    }
+}
